334da171f4d7a731cb5befcab2f55d7f1456fa7b
CWSODA <76740111+CWSODA@users.noreply.github.com>
2026-09-20 04:43:43 +0100

    moved log files to dedicated directory for ease of access

--- logger.hpp
+++ logger.hpp
@@ -2,27 +2,27 @@
 #include <fstream>
 
 inline std::ofstream& log() {
-    static std::ofstream log("log.txt");
+    static std::ofstream log("../logs/log.txt");
     return log;
 }
 #define LOG_LINE(s) log() << s << std::endl
 
 inline std::ofstream& log_sp() {
-    static std::ofstream log("stack_pointer_log.txt");
+    static std::ofstream log("../logs/stack_pointer_log.txt");
     return log;
 }
 #define LOG_SP_LINE(s)
 // #define LOG_SP_LINE(s) log_sp() << std::hex << s << std::endl
 
 inline std::ofstream& log_mem() {
-    static std::ofstream log("mem_log.txt");
+    static std::ofstream log("../logs/mem_log.txt");
     return log;
 }
 #define LOG_MEM_LINE(s)
 // #define LOG_MEM_LINE(s) log_sp() << std::hex << s << std::endl
 
 inline std::ofstream& log_misc() {
-    static std::ofstream log("misc_log.txt");
+    static std::ofstream log("../logs/misc_log.txt");
     return log;
 }
 // #define LOG_MISC_LINE(s)
